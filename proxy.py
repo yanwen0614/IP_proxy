fbe3d30deaae7f3a21e658e38caf9b6cb981566f
yanwen0614 <yanwen0614@gmail.com>
2017-03-12 23:05:24 +0800

    freeProxyFirst and saveProxy2file finished

--- proxy.py
+++ proxy.py
@@ -1,9 +1,10 @@
-import bs4
+# import bs4
+import time
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import requests
-import re
+# import re
 from lxml import etree
-# 
-
 
 class proxy(object):
     '''
@@ -18,19 +19,24 @@ class proxy(object):
     'Accept-Encoding': 'gzip, deflate, sdch',
     'Accept-Language': 'zh-CN,zh;q=0.8',
     }
-        
 
-
-    def getHtmlTree(self, url):
+    def getHtmlTree(self, url,  driver=None):
         """
         获取html树
         :param url:
         :param kwargs:
         :return:
         """
-        html = requests.get(url=url, headers=self.HEADER, timeout=30).content
-        return etree.HTML(html)
-
+        if driver:
+            driver.get(url)
+            time.sleep(1)
+            html = driver.page_source
+            return etree.HTML(html)
+        else:
+            r = requests.get(url, headers=self.HEADER)
+            html = r.content
+            return etree.HTML(html)
+# //*[@id="index_free_list"]/table/tbody
     def freeProxyFirst(self, page=10):
         """
         抓取快代理IP http://www.kuaidaili.com/
@@ -39,9 +45,12 @@ class proxy(object):
         """
         url_list = ('http://www.kuaidaili.com/proxylist/{page}/'.format(page=page) for page in range(1, page + 1))
         # 页数不用太多， 后面的全是历史IP， 可用性不高
-
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.userAgent"] = self.HEADER
+        driver = webdriver.PhantomJS( desired_capabilities=dcap)
+        #driver = webdriver.Chrome() # desired_capabilities=dcap
         for url in url_list:
-            tree = self.getHtmlTree(url)
+            tree = self.getHtmlTree(url, driver)
             proxy_list = tree.xpath('.//div[@id="index_free_list"]//tbody/tr')
             for proxy in proxy_list:
                 yield ':'.join(proxy.xpath('./td/text()')[0:2])
@@ -53,19 +62,25 @@ class proxy(object):
     def DelProxyFromDB():
         pass
     
-    def saveProxy():
+    def saveProxy(IP):
+
         pass    
 
     def saveProxy2DB():
         pass
 
-    def saveProxy2file():
-        pass
+    def saveProxy2file(self, IP):
+        now_time = time.ctime()
+        now_time = now_time.split()
+        now_time = '{year}-{month}-{day}'.format(year=now_time[-1],month=now_time[1],day=now_time[2])
+        with open(".\Proxy_Pool\{nowtime}.txt".format(nowtime = now_time),"a") as f:
+            f.write(IP+'\n')
+        
 
 def main():
     p = proxy()
     for pp in p.freeProxyFirst():
-        print(pp)
+        p.saveProxy2file(pp)
 
 if __name__ == '__main__':
     main()
