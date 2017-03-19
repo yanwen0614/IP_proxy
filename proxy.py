10c7a0464994db790a1126d7ed0e2f0353681010
yanwen0614 <yanwen0614@gmail.com>
2017-03-19 21:46:20 +0800

    add 66ip api

--- proxy.py
+++ proxy.py
@@ -11,14 +11,15 @@ class proxy(object):
     proxy
     '''
     def __init__(self):
-        self.HEADER = {'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko)',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'zh-CN,zh;q=0.8',
-    }
+        self.HEADER = {
+                        'Connection': 'keep-alive',
+                        'Cache-Control': 'max-age=0',
+                        'Upgrade-Insecure-Requests': '1',
+                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko)',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                        'Accept-Encoding': 'gzip, deflate, sdch',
+                        'Accept-Language': 'zh-CN,zh;q=0.8',
+                       }
 
     def getHtmlTree(self, url,  driver=None):
         """
@@ -50,23 +51,41 @@ class proxy(object):
         driver = webdriver.PhantomJS( desired_capabilities=dcap)
         #driver = webdriver.Chrome() # desired_capabilities=dcap
         for url in url_list:
+            print(url)
             tree = self.getHtmlTree(url, driver)
             proxy_list = tree.xpath('.//div[@id="index_free_list"]//tbody/tr')
             for proxy in proxy_list:
                 yield ':'.join(proxy.xpath('./td/text()')[0:2])
 
+    def freeProxySecond(proxy_number=100):
+        """
+        抓取代理66 http://www.66ip.cn/
+        :param proxy_number: 代理数量
+        :return:
+        """
+        url = "http://m.66ip.cn/mo.php?sxb=&tqsl={}&port=&export=&ktip=&sxa=&submit=%CC%E1++%C8%A1&textarea=".format(proxy_number)
+        html = requests.get(url, headers=HEADER).content
+        for proxy in re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}', html):
+            yield proxy
 
-    def VaildProxy():
-        pass
+    def VaildProxy(self,IP):
+        proxies = {
+                    "http": "http://{0}".format(IP),
+                    "https": "http://{0}".format(IP),
+                    }
+        try:
+            r = requests.get('https://www.baidu.com/', proxies=proxies, timeout=30, verify=False)
+            if r.status_code == 200:
+                return True
+            else:
+                return False
+        except Exception:
+            return False
     
     def DelProxyFromDB():
         pass
-    
-    def saveProxy(IP):
 
-        pass    
-
-    def saveProxy2DB():
+    def saveProxy2DB(self, IP):
         pass
 
     def saveProxy2file(self, IP):
@@ -75,12 +94,17 @@ class proxy(object):
         now_time = '{year}-{month}-{day}'.format(year=now_time[-1],month=now_time[1],day=now_time[2])
         with open(".\Proxy_Pool\{nowtime}.txt".format(nowtime = now_time),"a") as f:
             f.write(IP+'\n')
-        
+            #print(IP+'\n')
+
 
 def main():
     p = proxy()
-    for pp in p.freeProxyFirst():
-        p.saveProxy2file(pp)
+    for pp in p.freeProxyFirst(10):
+        if p.VaildProxy(pp):
+            print(pp+'  pass')
+            p.saveProxy2file(pp)
+        else:
+            print(pp+'  refuse')
 
 if __name__ == '__main__':
     main()
